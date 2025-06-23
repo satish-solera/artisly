658a7fd8850ed0ef66c563e000475a131ffe0e29
abc <satishsolera54@gmail.com>
2025-06-23 16:31:57 +0530

    artistly-assignment-complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,8 @@ export default function Home() {
       <ArtistInfo />
 
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-4">
+      <div className="grid grid-cols-1 
+      sm:grid-cols-2 md:grid-cols-3 gap-6 my-4">
 
         {artist.map((data) => (
           <ArtistCard
